8e6f9756c47b838bd2c43bd675c825d8c227e1a0
devansh-lodha <devanshlodha12@gmail.com>
2025-01-17 09:51:46 +0000

    Deploying to gh-pages from @ devansh-lodha/devansh-lodha.github.io@c0f08aa0141c4f07cb1881a59b3df4f475762bc4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,18 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "post-histogram-equalization-and-matching-devansh-s-blog",
-      
-        title: 'Histogram Equalization and Matching – Devansh’s Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "This post explores histogram matching and equalization between images of Elon Musk, Lenna, a Panda and the IIT Gandhinagar campus.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://devansh-lodha.github.io/blog/posts/histogram_matching/histogram_matching.html", "_blank");
-        
-      },
-    },{id: "news-made-it-to-semester-1-s-dean-s-list-letter-of-appreciation",
+        },{id: "news-made-it-to-semester-1-s-dean-s-list-letter-of-appreciation",
           title: 'Made it to Semester-1’s Dean’s List! (Letter of Appreciation)',
           description: "",
           section: "News",},{id: "news-made-it-to-semester-2-s-dean-s-list-letter-of-appreciation",
@@ -91,13 +80,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/devanshlodha", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-whatsapp',
